c4c6a6565e084c6b8a43b2aaf981b839d4986b62
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2024-11-24 07:26:55 +0200

    finished the blockchain class

--- src/Block.java
+++ src/Block.java
@@ -1,55 +1,89 @@
+import java.security.MessageDigest;
 import java.util.Date;
 
-public class Block{
+public class Block {
 
-private int blockIndex;
-private String  data;
-private String prevHash;
-private String  blockHash;
-private Long timeStamps= new Date().getTime();
-private int nonce;
+    private int blockIndex;
+    private String data;
+    private String prevHash;
+    private String blockHash;
+    private Long timeStamps = new Date().getTime();
+    private int nonce;
 
-public Block(int index,String data,String prevHash){
-    this.blockIndex= index;
-    this.data= data;
-    this.nonce=0;
-    this.prevHash= prevHash;
-    this.blockHash= calculateBlockHash();
-    
+    public Block(int index, String data, String prevHash) {
+        this.blockIndex = index;
+        this.data = data;
+        this.nonce = 0;
+        this.prevHash = prevHash;
+        this.blockHash = calculateBlockHash();
+
     }
-    
+
     public String calculateBlockHash() {
-        String input= blockIndex  + prevHash + nonce + data + timeStamps;
+        String input = blockIndex + prevHash + nonce + data + timeStamps;
 
-        return input;
+        return applySHA256(input);
+    }
+    private String applySHA256(String input) {
+        try {
+            MessageDigest sha = MessageDigest
+                    .getInstance(
+                            "SHA-256");
+            int i = 0;
+
+            byte[] hash = sha.digest(
+                    input.getBytes("UTF-8"));
+
+            // hexHash will contain
+            // the Hexadecimal hash
+            StringBuffer hexHash = new StringBuffer();
+
+            while (i < hash.length) {
+                String hex = Integer.toHexString(
+                        0xff & hash[i]);
+                if (hex.length() == 1)
+                    hexHash.append('0');
+                hexHash.append(hex);
+                i++;
+            }
+
+            return hexHash.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
+    public void  mineBLock(int difficultLevel){
 
-    //getters & setter
+        String target= new String( new char[difficultLevel]).replace('\0', '0');
 
-    public String getPrevHash(){
+        while (!blockHash.substring(0, difficultLevel).equals(target)) {
+            nonce++; 
+            blockHash = calculateBlockHash();  
+        }
+
+        System.out.println("Block mined: " + blockHash);
+    }
+
+    // getters & setter
+
+
+    public String getPrevHash() {
         return prevHash;
     }
 
-    public String getBlockHash(){
+    public String getBlockHash() {
         return prevHash;
     }
 
-   
-
-
-    public void setPrevHash(String newPrevHash){
-        this.prevHash=newPrevHash;
+    public void setPrevHash(String newPrevHash) {
+        this.prevHash = newPrevHash;
     }
 
-    public void setBLockHash(String newBlockHash){
-      this.blockHash= newBlockHash;
+    public void setBLockHash(String newBlockHash) {
+        this.blockHash = newBlockHash;
     }
-
-
-    
-
-
 
 }
